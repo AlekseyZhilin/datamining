f8208bcca4144a5fcf6ae0d6a4d620c9884a2f9e
Aleksey <alekseyzhilin@mail.ru>
2021-04-18 10:32:27 +0300

    add Lesson1

---
+++ Lesson1.py
@@ -0,0 +1,50 @@
+import time
+import json
+from pathlib import Path
+import requests
+
+
+class Parse5ka:
+    headers = {"User-Agent": "Aleksey"}
+
+    def __init__(self, start_url: str, save_path: Path):
+        self.start_url = start_url
+        self.save_path = save_path
+
+    def _get_response(self, url):
+        while True:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                return response
+            time.sleep(1)
+
+    def run(self):
+        for product in self._parse(self.start_url):
+            product_path = self.save_path.joinpath(f"{product['id']}.json")
+            self._save(product, product_path)
+
+    def _parse(self, url: str):
+        while url:
+            response = self._get_response(url)
+            data: dict = response.json()
+            url = data["next"]
+            for product in data["results"]:
+                yield product
+
+    def _save(self, data: dict, file_path: Path):
+        file_path.write_text(json.dumps(data, ensure_ascii=False), encoding="utf-8")
+
+
+def get_save_path(dir_name):
+    save_path = Path(__file__).parent.joinpath(dir_name)
+    if not save_path.exists():
+        save_path.mkdir()
+    return save_path
+
+
+if __name__ == "__main__":
+    url = "https://5ka.ru/api/v2/special_offers/"
+    cat_url = "https://5ka.ru/api/v2/categories/"
+    save_path_products = get_save_path("products")
+    parser_products = Parse5ka(url, save_path_products)
+    parser_products.run()
