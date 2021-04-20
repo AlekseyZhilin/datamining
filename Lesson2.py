39fd58522ace13dd2645516ff6d4aa5b47146935
Aleksey <alekseyzhilin@mail.ru>
2021-04-20 23:57:48 +0300

    change Lesson2

--- Lesson2.py
+++ Lesson2.py
@@ -1,6 +1,5 @@
 import requests
 import bs4
-from pathlib import Path
 from urllib.parse import urljoin
 import pymongo
 import time
@@ -57,16 +56,8 @@ class MagnitParse:
             print(itm)
 
 
-def get_save_path(dir_name):
-    dir_path = Path(__file__).parent.joinpath(dir_name)
-    if not dir_path.exists():
-        dir_path.mkdir()
-    return dir_path
-
-
 if __name__ == "__main__":
     url = "https://magnit.ru/promo/"
-    save_path = get_save_path("magnit_product")
     db_client = pymongo.MongoClient("mongodb://localhost:27017")
     parser = MagnitParse(url, db_client)
     parser.run()
